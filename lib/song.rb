876e4c876d8c7a6fbe4497e658c5ef803e7af62b
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-26 22:57:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
   attr_accessor :name, :artist, :genre
+  @@count=
   def initialize(name, artist, genre)
    @name=name
    @artist=artist
    @genre=genre
   end
-  def name
-  end
+  
 end 
