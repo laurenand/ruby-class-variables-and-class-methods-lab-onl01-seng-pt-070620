2fda39b7d48cc04409b75eba9dc52adee81ca7cd
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-26 23:59:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,5 +34,14 @@ class Song
     genre_count
   end
   def self.artist_count
+    genre_count={}
+    @@genres.each do |genre|
+      if genre_count.key?(genre)
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end 
 end 
